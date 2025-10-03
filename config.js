f0247457aef6cac47596084839827e5507a71f62
Doman <nanodoman@gmail.com>
2025-10-03 21:51:39 +0200

    refactor: reorganize project
    - added enums.js
    - merge scripts into single main.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const VERSION = '2.5.0-alpha';
-
-console.log(`Asteroids JS – version ${VERSION}`);
-
-document.querySelector('meta[name="version"]').setAttribute('content', VERSION);
-document.querySelectorAll('[data-version]').forEach((node) => {
-  node.innerText = VERSION;
-});
